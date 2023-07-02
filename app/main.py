a03e56a9cde4856a249cb240e11097d2ab83df83
Vadim <yagofarov.vadim@mail.ru>
2023-07-02 23:17:06 +0500

    first commit

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Query
+from typing import Optional
+from datetime import date
+
+app = FastAPI()
+
+@app.get('/hotels')
+def get_hotels(location: str,
+               date_from: date,
+               date_to: date,
+               stars: Optional[int] = Query(None, ge=1, le=5),
+               has_spa: Optional[bool] = None
+               ):
+    return date_from, date_to
+
+
+# Press the green button in the gutter to run the script.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
